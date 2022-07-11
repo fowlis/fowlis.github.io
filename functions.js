c3d4d5ff57631f003aaa07980df79eedfb310348
fowlis <fowlis@protonmail.com>
2022-07-11 20:48:06 +0100

    cookiecalc (wip) and other things

---
+++ functions.js
@@ -0,0 +1,47 @@
+function calculateAmountinTime() {
+  document.getElementById("output1").innerHTML = "";
+  var x = document.getElementById("cps").value;
+  var y = document.getElementById("time").value;
+
+  let formatvar = "";
+  let format = document.forms[0];
+  let i;
+
+  for (i = 0; i < format.length; i++) {
+    if (format[i].checked) {
+      formatvar = formatvar + format[i].value + "";
+    }
+  }
+
+  console.log(formatvar);
+
+  if ((timeformat = "seconds")) {
+    document.getElementById("output1").innerHTML =
+      "You will make " + (z = y * x) + " cookies in " + y + " seconds!";
+    console.log("1 run succ");
+    return;
+  } else if ((timeformat = "minutes")) {
+    document.getElementById("output1").innerHTML =
+      "You will make " + (z = y * 60 * x) + " cookies in " + y + " minutes!";
+    console.log("2 run succ");
+    return;
+  } else if ((timeformat = "hours")) {
+    document.getElementById("output1").innerHTML =
+      "You will make " + (z = y * 60 * 60 * x) + " cookies in " + y + " hours!";
+    console.log("3 run succ");
+    return;
+  }
+}
+
+function calculateTimeforAmount() {
+  var x = document.getElementById("cps2").value;
+  var y = document.getElementById("amount").value;
+
+  z = x * 60;
+  console.log(z);
+
+  a = y / z;
+  console.log(a);
+  document.getElementById("output2").innerHTML =
+    "It will take " + a + " minutes to make " + y + " cookies!";
+}
