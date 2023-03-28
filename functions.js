db53822d5a888f89e9aad430ea6d02cc91b08596
fowlis <fowlis@protonmail.com>
2023-03-28 16:25:39 +0100

    aaaa

--- functions.js
+++ functions.js
@@ -113,4 +113,4 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-} 
+}
